507a09ecf5fa5021e857d01c55a39d21a372eefb
gianmarco <gianmarco.culqui.ikaros@gmail.com>
2022-05-04 15:13:46 +0200

    uploading

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <titolo-cli/>
+   
+    
+  </div>
+
+</template>
+
+<script>
+import titoloCli from './components/titoloCli.vue'
+
+export default {
+  name: 'App',
+  components: {
+    titoloCli,
+    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/titoloCli.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>nuono titolo di VUE CLI FIRST</h1>
+
+</template>
+
+<script>
+export default {
+  name: 'titoloCli',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
